c23560b14ea3544b9f3b44d1615c85b0f6f9e52b
Septicloud <sptwn26@upi.edu>
2025-12-12 22:53:00 +0700

    revisi dikit

--- galeri.php
+++ galeri.php
@@ -29,9 +29,9 @@ function getYouTubeEmbedUrl($url) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeri - SLB Roza</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1>/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
     <style>
         /* CSS KHUSUS HALAMAN GALERI (Bisa dipindah ke file style.css) */
         .section-header {
